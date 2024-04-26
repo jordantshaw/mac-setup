c2d63013c64f004a37ddb5f12b95f9a35c155205
Jordan Shaw <33508069+jordantshaw@users.noreply.github.com>
2024-04-25 23:38:24 -0600

    Updating init script

--- scripts/init-pyenv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Init pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-echo 'eval "$(pyenv init -)"' >> ~/.zshrc
-eval "$(pyenv virtualenv-init -)"
